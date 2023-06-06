7b6f1485a7a9774a88e8d011a815d6f33b076cfa
Dylan3009 <dylan.melendezl@hotmail.com>
2023-06-06 11:55:49 -0600

    Add: Transform View

--- client/arixmljson/src/App.js
+++ client/arixmljson/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import Transform from './components/Transform';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data to JSON</h1>
+      <Transform />
+      <h1>Data to XML</h1>
+      <Transform />
     </div>
+    </>
   );
 }
 
---
+++ client/arixmljson/src/components/Transform.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Transform.css'
+
+const Transform = () => {
+  return (
+    <div>
+        <button className='create-button'>Create</button>
+        <button className='load-button'>Load</button>
+    </div>
+  )
+}
+
+export default Transform
