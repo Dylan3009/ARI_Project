25695c48fbcc8eeaf45e5b7e4370a2541c64acd9
Dylan3009 <dylan.melendezl@hotmail.com>
2023-06-14 01:42:21 -0600

    Add: Better UI, ReadOnly txt and json in TxtToJson page and Download Button

--- client/arixmljson/src/App.js
+++ client/arixmljson/src/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
-import Generate from './Pages/Generate';
+import JsonToText from './Pages/Converters/JsonToText';
+import TextToJson from './Pages/Converters/TextToJson';
+import XmlToText from './Pages/Converters/XmlToText';
+import TextToXml from './Pages/Converters/TextToXml';
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />}></Route>
-          <Route path='/convert/json' element={<Generate />}></Route>
+          <Route path="/convert/json" element={<JsonToText/>}></Route>
+          <Route path="/convert/json/text" element={<TextToJson/>}></Route>
+          <Route path="/convert/xml" element={<XmlToText/>}></Route>
+          <Route path="/convert/xml/text" element={<TextToXml/>}></Route>
         </Routes>
       </Router>
     </>
--- server/ariback/src/datajson/dto/update-datajson.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDatajsonDto } from './create-datajson.dto';
-
-export class UpdateDatajsonDto extends PartialType(CreateDatajsonDto) {}
