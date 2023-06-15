ec1d2c5079d0dfbaec0ae61d0b4dcad20b7a6a7b
Dylan3009 <dylan.melendezl@hotmail.com>
2023-06-14 22:17:51 -0600

    Update: Name bug

--- client/arixmljson/src/Pages/Home.js
+++ client/arixmljson/src/Pages/Home.js
@@ -6,11 +6,11 @@ const Home = () => {
   return (
     <div className="home">
       <h1>Text to JSON --- JSON to Text</h1>
-      <a href="/convert/json">JSON to Text</a>
-      <a href="/convert/json/text">Text to JSON</a>
+      <a href="/convert/json">Text to JSON</a>
+      <a href="/convert/json/text">JSON to Text</a>
       <h1>Text to XML --- XML to Text</h1>
-      <a href="/convert/xml">XML to Text</a>
-      <a href="/convert/xml/text">Text to XML</a>
+      <a href="/convert/xml">Text to XML</a>
+      <a href="/convert/xml/text">XML to Text</a>
     </div>
   )
 }
---
+++ server/ariback/src/datajson/jsontxt.controller.ts
@@ -0,0 +1,55 @@
+import { Controller, Get, Post, Req, Res, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as fs from 'fs';
+import { Express } from 'express';
+import * as multer from 'multer';
+import { extname, join } from 'path';
+//import * as path from 'path';
+
+@Controller('convert')
+export class JsonTextController {
+
+    @Post('json/text')
+    @UseInterceptors(FileInterceptor('file',
+        {
+            storage: multer.diskStorage({
+                destination: './src/files',
+                filename: (req, file, cb) => {
+                    const extension = extname(file.originalname);
+                    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+                    const filename = `${uniqueSuffix}${extension}`;
+                    cb(null, filename);
+                }
+            })
+        }
+    ))
+    async convertToJson(@UploadedFile() file: Express.Multer.File, @Res() response) {
+
+        const filename: string = `./src/files/${file.filename}`;
+        const fileContent = fs.readFileSync(filename, 'utf-8').toString();
+        const jsonData: any[] = JSON.parse(fileContent);
+
+        let txtContent = "documento,nombre,apellido,tarjeta,tipo,telefono,poligono\n";
+
+        jsonData.forEach(item => {
+            const row = [
+                item.documento,
+                item.nombre,
+                item.apellido,
+                item.tarjeta,
+                item.tipo,
+                item.telefono,
+                ...item.poligono
+            ].join(",");
+
+            txtContent += row + "\n";
+        });
+
+        console.log(txtContent);
+        const outputFilename = filename.replace(".json", ".txt");
+        fs.writeFileSync(outputFilename, txtContent, "utf-8");      
+
+        response.send(txtContent);
+
+    }
+}
