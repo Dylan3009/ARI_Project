0038954d38f6210b16d83d22b27ab766cc69ebb6
MJZF00155619 <00155619@uca.edu.sv>
2023-06-15 01:02:10 -0600

    Feat: Adding Convert XmlToTxt

--- server/ariback/src/datajson/datajson.module.ts
+++ server/ariback/src/datajson/datajson.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatajsonService } from './datajson.service';
 import { DatajsonController } from './datajson.controller';
 import { JsonTextController } from './jsontxt.controller';
+import { XmlToTxtController } from './xmlToTextController';
+
 
 @Module({
-  controllers: [DatajsonController, JsonTextController],
+  controllers: [DatajsonController, JsonTextController, XmlToTxtController],
   providers: [DatajsonService]
 })
 export class DatajsonModule {}
